5c63b1e5ee0d0338c7a8e6c5f75ee04525c073e0
takutossan <gikushaku.4989@gmail.com>
2021-07-27 16:39:10 +0900

    手順4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
-  validates :occupation, presence: true
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 
   has_many :products
   has_many :buyers
