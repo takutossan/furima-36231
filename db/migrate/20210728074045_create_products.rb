3a5ad69c177408b3aef655d29179d820b603337c
takutossan <gikushaku.4989@gmail.com>
2021-07-28 17:54:45 +0900

    手順2

--- db/migrate/20210728074045_create_products.rb
+++ db/migrate/20210728074045_create_products.rb
@@ -3,13 +3,13 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string :name,                 null: false
       t.text :message,                null: false
-      t.integer :category_id          null: false
-      t.integer :price                null: false
-      t.integer :item_status_id       null: false
-      t.integer :shipping_cost_id     null: false
-      t.integer :shipper_id           null: false
-      t.integer :shipping_date_id     null: false
-      t.references :user              null: false, foreign_key: true
+      t.integer :category_id,         null: false
+      t.integer :price,               null: false
+      t.integer :item_status_id,      null: false
+      t.integer :shipping_cost_id,    null: false
+      t.integer :shipper_id,          null: false
+      t.integer :shipping_date_id,    null: false
+      t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
   end
