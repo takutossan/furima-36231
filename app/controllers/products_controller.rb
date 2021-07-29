ee99b5508221b63fb2cc853635655977faa5bbc0
takutossan <gikushaku.4989@gmail.com>
2021-07-29 11:00:41 +0900

    手順6

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
   end
 
@@ -9,8 +9,19 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :image, :message, :category_id, :price, :item_status_id, :shipping_cost_id, :shipper_id, :shipping_date_id, :user).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,5 @@ class Product < ActiveHash::Base
   belongs_to :user
 
   with_options presence: true do
-  validates :name
-  validates :message
-  validates :category_id
-  validates :price
-  validates :item_status_id
-  validates :shipping_cost_id
-  validates :shipper_id
-  validates :shipping_date_id
-  validates :user
+  validates :c
 end
