e0d3a9198c801a70eeb3d35f69e56918653c3409
takutossan <gikushaku.4989@gmail.com>
2021-07-29 10:25:48 +0900

    手順4

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveHash::Base
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+
+  with_options presence: true do
+  validates :name
+  validates :message
+  validates :category_id
+  validates :price
+  validates :item_status_id
+  validates :shipping_cost_id
+  validates :shipper_id
+  validates :shipping_date_id
+  validates :user
 end
