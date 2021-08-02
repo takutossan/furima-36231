e78c586f153412419e50fa70778f25c209085b64
takutossan <gikushaku.4989@gmail.com>
2021-08-02 11:23:11 +0900

    手順1,2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    # @products = Product.all
+     @products = Product.all
   end
 
   def new
