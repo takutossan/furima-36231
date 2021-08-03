5d5d39d473304a013e75e62b887fa2c1714e750e
takutossan <gikushaku.4989@gmail.com>
2021-08-03 15:41:20 +0900

    手順1〜3の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :set_confirmation, only: [:edit, :update, :destroy]
+  before_action :set_confirmation, only: [:edit, :update]
   
   def index
     @products = Product.all.order('created_at DESC')
@@ -21,7 +21,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product.destroy
+    if @product.destroy
+      redirect_to root_path
+    end
   end
 
   def show
@@ -53,4 +55,5 @@ class ProductsController < ApplicationController
     unless current_user.id == @product.user.id
       redirect_to root_path
    end
+  end
 end
