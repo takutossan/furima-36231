a8c32651fd849106b847bc9de30fb247dc001b91
takutossan <gikushaku.4989@gmail.com>
2021-08-03 15:10:54 +0900

    修正依頼1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :set_confirmation, only: [:edit, :update]
+  
   def index
     @products = Product.all.order('created_at DESC')
   end
@@ -22,18 +24,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    unless current_user.id == @product.user.id
-    redirect_to root_path
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
     @product.update(product_params)
     if @product.save
       redirect_to root_path
@@ -47,4 +43,13 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :image, :message, :category_id, :price, :item_status_id, :shipping_cost_id,:shipper_id, :shipping_date_id, :user).merge(user_id: current_user.id)
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_confirmation 
+    unless current_user.id == @product.user.id
+      redirect_to root_path
+   end
 end
