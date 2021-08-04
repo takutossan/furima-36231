a083f2b1beb7671284b7817584f37024f29d25a6
takutossan <gikushaku.4989@gmail.com>
2021-08-04 16:55:46 +0900

    Step2手順1

---
+++ app/javascript/card.js
@@ -0,0 +1,29 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_866cdc5fdb2239c0613bba99");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        debugger;
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
