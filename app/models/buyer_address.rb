c982d8411ea219c4bd69fda7d180491afbe1b798
takutossan <gikushaku.4989@gmail.com>
2021-08-04 15:08:28 +0900

    手順4〜5

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,9 +1,9 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :user, :product, :zip_code, :shipper_id, :municipalities, :street_number, :telephone_number, :building, :buyer
+  attr_accessor :user_id, :product, :zip_code, :shipper_id, :municipalities, :street_number, :telephone_number, :building, :buyer
 
   with_options presence: true do
-    validates :user
+    validates :user_id
     validates :product
     validates :zip_code
     validates :shipper_id
@@ -13,7 +13,8 @@ class BuyerAddress
     validates :buyer
 
     def save
-      buyer = Buyer.create(user: user, product: product)
+      buyer = Buyer.create(user: user_id, product: product)
       Address.create(zip_code: zip_code, shipper_id: shipper_id, municipalities: municipalities, street_number: street_number, telephone_number: telephone_number, building: building, buyer: buyer)
     end
+  end
 end
